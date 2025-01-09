5fff84cc6e3a7084a106651e6f33eb3a43665a81
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-01-09 16:57:38 -0600

    Adding code

--- fibonacci.rs
+++ fibonacci.rs
@@ -12,10 +12,22 @@ fn main() {
     io::stdin().read_line(&mut input_string).expect("Failed to read input");
 
     // Convert the input to an integer
-    let input: u32 = input_string.trim().parse::<u32>().expect("Failed to convert input to integer");
+    let quantity_to_produce: u32 = input_string.trim().parse::<u32>().expect("Failed to convert input to integer");
     
+    // Generate the Fibonacci numbers
+    let sequence: Vec<u32> = Vec::new();
 
-
+    let generate_next_fibonacci_num(current_num: u32) -> u32 {
+        while sequence.len() < quantity_to_produce {
+            match current_num {
+                0 | 1 => 1,
+                _ => sequence[-1] + sequence[-2]
+    
+            }
+            sequence.push(generate_next_fibonacci_num(sequence.len()));
+        }
+        
+    }
 
 
 }
