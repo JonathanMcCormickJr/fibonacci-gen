4879b3bba5ed82d4ccc989f5a794fcd8572c96bc
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-01-09 18:20:05 -0600

    Eliminating initial string allocation by going directly from stdin->variable

--- fibonacci.rs
+++ fibonacci.rs
@@ -5,29 +5,33 @@
 use std::io;
 
 fn main() {
-
     // Get the quantity of numbers to generate
-    let mut input_string: String = String::new();
     println!("Enter the number of terms you want to generate: ");
-    io::stdin().read_line(&mut input_string).expect("Failed to read input");
-
-    // Convert the input to an integer
-    let quantity_to_produce: u32 = input_string.trim().parse::<u32>().expect("Failed to convert input to integer");
-    
+    let quantity_to_produce: u32 = io::stdin()
+        .lines()
+        .next()
+        .expect("No input received")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Failed to parse number");
     // Generate the Fibonacci numbers
     let sequence: Vec<u32> = Vec::new();
 
     let generate_next_fibonacci_num(current_num: u32) -> u32 {
-        while sequence.len() < quantity_to_produce {
-            match current_num {
-                0 | 1 => 1,
-                _ => sequence[-1] + sequence[-2]
-    
-            }
-            sequence.push(generate_next_fibonacci_num(sequence.len()));
+
+        if sequence.len() == 0 {
+            sequence.push(0);
+        } else if sequence.len() == 1 | sequence.len() == 2 {
+            sequence.push(1);
+        } else {
+            sequence.push(sequence[-1] + sequence[-2]);
         }
-        
+
+        if sequence.len() < quantity_to_produce {
+            generate_next_fibonacci_num(sequence[-1]);
+        } 
     }
 
+    generate_next_fibonacci_num(quantity_to_produce)
 
 }
