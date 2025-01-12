70e7f7b630ad4b0fb75ee720b6fd0a8b507ffadf
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-01-12 16:26:44 -0600

    Adding first test

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,40 @@
 #![forbid(unsafe_code)]
 
 fn main() {
-
-
-    fn generate_nums(quantity: u32) -> Vec<u32> {
-        let mut additional_iterations_needed = quantity;
-        let mut sequence: Vec<u32> = Vec::new();
-
-        loop {
-            if additional_iterations_needed < 1 {
-                break
-            }
-
-            if sequence.len() == 0 {
-                sequence.push(0);
-            } else if sequence[sequence.len() - 1] == 0 || sequence[sequence.len() - 1] == 1 && sequence[sequence.len() - 2] == 0 {
-                sequence.push(1);
-            } else {
-                sequence.push(sequence[sequence.len() - 1] + sequence[sequence.len() - 2])
-            }
-
-            additional_iterations_needed -= 1;
-        };
-
-        sequence
-    }
-
     println!("{:?}", generate_nums(10));
+}
 
 
+pub fn generate_nums(quantity: u32) -> Vec<u32> {
+    let mut additional_iterations_needed = quantity;
+    let mut sequence: Vec<u32> = Vec::new();
 
+    loop {
+        if additional_iterations_needed < 1 {
+            break
+        }
+
+        if sequence.len() == 0 {
+            sequence.push(0);
+        } else if sequence[sequence.len() - 1] == 0 || sequence[sequence.len() - 1] == 1 && sequence[sequence.len() - 2] == 0 {
+            sequence.push(1);
+        } else {
+            sequence.push(sequence[sequence.len() - 1] + sequence[sequence.len() - 2])
+        }
+
+        additional_iterations_needed -= 1;
+    };
+
+    sequence
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_generate_nums() {
+        let example0 = generate_nums(0);
+        assert_eq!(example0, Vec::<u32>::new())
+    }
 }
